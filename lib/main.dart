9a22c8a28ba1c03ded70795ae245c0b3a185e33e
Twinkle <Twinkle.soboft@gmail.com>
2022-11-11 19:00:26 +0530

    HomeExpanded UI Design(Navigation Drawer)

--- lib/main.dart
+++ lib/main.dart
@@ -11,105 +11,136 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final Scaffoldkey = GlobalKey<ScaffoldState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<String> DrawerName = [
+    "Home",
+    "Search",
+    "Create Trips",
+    "Add",
+    "Trips",
+    "Notifications",
+    "Setting",
+    "Logout"
+  ];
+  List<IconData> DrawerIcon = [
+    Icons.home,
+    Icons.search,
+    Icons.trip_origin,
+    Icons.add,
+    Icons.fire_truck,
+    Icons.notifications,
+    Icons.settings,
+    Icons.logout
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      key: Scaffoldkey,
+      appBar: AppBar(),
+      drawer: Drawer(
+        elevation: 5,
+        width: MediaQuery.of(context).size.width * 0.9,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.only(left: 26),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: 20),
+                    height: 150,
+                    width: 150,
+                    child: CircleAvatar(
+                      radius: 30.0,
+                      backgroundImage:
+                          AssetImage('assets/images/AppIcon.png'),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          "Jeniffer Winget",
+                          style: TextStyle(
+                              fontSize: 13, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              Align(alignment: Alignment(10, 10)),
+                              Text(
+                                "+91 123412349",
+                                style: TextStyle(fontSize: 10),
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                "jenny@gmail.com",
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.indigoAccent,
+                          // background (button) color
+                        ), // foreground (text) color
+                        onPressed: () => print('pressed'),
+                        child: Text('Edit Profile '),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              ListView.builder(
+                itemCount: DrawerName.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return commondrawername(
+                      icon: DrawerIcon[index], text: DrawerName[index]);
+                },
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget commondrawername({String? text, required IconData icon}) {
+    return Container(
+      child: ExpansionTile(
+          trailing: SizedBox.shrink(),
+          title: Text(text ?? ''),
+          leading: Icon(icon),
+          childrenPadding: EdgeInsets.only(left: 100)),
     );
   }
 }
